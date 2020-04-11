5719b7f18b5da1dc79111bdefae65df45715731d
MaysounRahhal <maysounrahhal@gmail.com>
2020-04-11 21:07:05 +0300

    Assignment 3

---
+++ week3.py
@@ -0,0 +1,13 @@
+
+i=0
+while True:
+    num1=7
+    print()
+    print()
+    print ('    Numbers from 1 to 10 ')
+    num= int(input('Guess the number :   '))
+    if (num1==num):
+        print ('  Greet you did it !')
+        break
+    else:
+        continue
